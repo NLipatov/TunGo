885b17a628c26ba886666a8301c4540909f77fdb
Nikita <6stringsohei@gmail.com>
2024-09-18 18:44:00 +0200

    moved server configuration into conf.json

--- src/server.go
+++ src/server.go
@@ -3,44 +3,45 @@ package main
 import (
 	"etha-tunnel/network"
 	"etha-tunnel/network/utils"
+	"etha-tunnel/settings/server"
 	"fmt"
 	"log"
 )
 
-const (
-	serverIfName = "ethatun0"
-	tunIP        = "10.0.0.1/24" //ToDo: move to server configuration file
-	listenPort   = ":8080"       //ToDo: move to server configuration file
-)
-
 func main() {
-	err := configureServer()
-	tunFile, err := network.OpenTunByName(serverIfName)
+	conf, err := (&server.Conf{}).Read()
+	if err != nil {
+		log.Fatalf("failed to read configuration: %v", err)
+	}
+	fmt.Print(conf.TCPPort)
+
+	err = configureServer(conf)
+	tunFile, err := network.OpenTunByName(conf.IfName)
 	if err != nil {
 		log.Fatalf("Failed to open TUN interface: %v", err)
 	}
 	defer tunFile.Close()
 
-	err = network.Serve(tunFile, listenPort)
+	err = network.Serve(tunFile, conf.TCPPort)
 	if err != nil {
 		log.Print(err)
 	}
 }
 
-func configureServer() error {
-	_, _ = utils.DelTun(serverIfName)
+func configureServer(conf *server.Conf) error {
+	_, _ = utils.DelTun(conf.IfName)
 
-	name, err := network.UpNewTun(serverIfName)
+	name, err := network.UpNewTun(conf.IfName)
 	if err != nil {
-		log.Fatalf("Failed to create interface %v: %v", serverIfName, err)
+		log.Fatalf("Failed to create interface %v: %v", conf.IfName, err)
 	}
 	fmt.Printf("Created TUN interface: %v\n", name)
 
-	_, err = utils.AssignTunIP(serverIfName, tunIP)
+	_, err = utils.AssignTunIP(conf.IfName, conf.IfIP)
 	if err != nil {
 		return err
 	}
-	fmt.Printf("Assigned IP %s to interface %s\n", tunIP, serverIfName)
+	fmt.Printf("Assigned IP %s to interface %s\n", conf.TCPPort, conf.IfName)
 
 	return nil
 }
---
+++ src/settings/server/conf.go
@@ -0,0 +1,41 @@
+package server
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+type Conf struct {
+	IfName  string `json:"IfName"`
+	IfIP    string `json:"IfIP"`
+	TCPPort string `json:"TCPPort"`
+}
+
+func (s *Conf) Read() (*Conf, error) {
+	confPath, err := getServerConfPath()
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := os.ReadFile(confPath)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(data, s)
+	if err != nil {
+		return nil, err
+	}
+
+	return s, nil
+}
+
+func getServerConfPath() (string, error) {
+	execPath, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	settingsPath := filepath.Join(filepath.Dir(execPath), "src", "settings", "server", "conf.json")
+	return settingsPath, nil
+}
