78cc7247f2cebd397739cd37dd883e905c37308c
Nikita <6stringsohei@gmail.com>
2024-09-22 12:43:23 +0200

    Revert "clientHello update"
    This reverts commit 6624f8b4b771038b0bb4a05416f1efcec6b280ff.

--- src/handshake/client/сlientHello.go
+++ src/handshake/client/сlientHello.go
@@ -1,7 +1,9 @@
 package client
 
 import (
+	"etha-tunnel/crypto/asymmetric/curve25519"
 	"fmt"
+	"log"
 )
 
 type ClientHello struct {
@@ -31,7 +33,7 @@ func (m *ClientHello) Read(data []byte) (*ClientHello, error) {
 	return m, nil
 }
 
-func (m *ClientHello) Write(ipVersion uint8, ip string, pubKey [32]byte) ([]byte, error) {
+func (m *ClientHello) Write(ipVersion uint8, ip string) ([]byte, error) {
 	if ipVersion != 4 && ipVersion != 6 {
 		return nil, fmt.Errorf("invalid ip version")
 	}
@@ -49,7 +51,12 @@ func (m *ClientHello) Write(ipVersion uint8, ip string, pubKey [32]byte) ([]byte
 	arr[1] = uint8(len(ip))
 	copy(arr[2:], ip)
 
-	copy(arr[2+len(ip):], pubKey[:])
+	_, publicKey, err := curve25519.GenerateCurve25519KeyPair()
+	if err != nil {
+		log.Fatalf("could not generate public key: %s", err)
+	}
+
+	copy(arr[2+len(ip):], publicKey[:])
 
 	return arr, nil
 }
