c41c97f4793e4298654b7f4bf9a0178e9e3c1149
Nikita <6stringsohei@gmail.com>
2024-09-22 12:43:25 +0200

    Revert "clientHello now also contains a pub key"
    This reverts commit bee64043d687e91b4cf7bb1fd802ec9fa1301b98.

--- src/handshake/сlientHello.go
+++ src/handshake/сlientHello.go
@@ -1,21 +1,16 @@
 package handshake
 
-import (
-	"etha-tunnel/crypto/asymmetric/curve25519"
-	"fmt"
-	"log"
-)
+import "fmt"
 
 type ClientHello struct {
 	IpVersion       uint8
 	IpAddressLength uint8
 	IpAddress       string
-	PublicKey       [32]byte
 }
 
 func (m *ClientHello) Read(data []byte) (*ClientHello, error) {
-	if len(data) < int(2+m.IpAddressLength+32) {
-		return nil, fmt.Errorf("invalid message length")
+	if len(data) < 4 {
+		return nil, fmt.Errorf("invalid message")
 	}
 
 	m.IpVersion = data[0]
@@ -26,37 +21,32 @@ func (m *ClientHello) Read(data []byte) (*ClientHello, error) {
 
 	m.IpAddressLength = data[1]
 
-	m.IpAddress = string(data[2 : 2+m.IpAddressLength])
+	if int(2+m.IpAddressLength) > len(data) {
+		return nil, fmt.Errorf("invalid IP address length")
+	}
 
-	copy(m.PublicKey[:], data[2+m.IpAddressLength:32+2+m.IpAddressLength])
+	m.IpAddress = string(data[2 : 2+m.IpAddressLength])
 
 	return m, nil
 }
 
-func (m *ClientHello) Write(ipVersion uint8, ip string) ([]byte, error) {
+func (m *ClientHello) Write(ipVersion uint8, ip string) (*[]byte, error) {
 	if ipVersion != 4 && ipVersion != 6 {
 		return nil, fmt.Errorf("invalid ip version")
 	}
 
-	if ipVersion == 4 && (len(ip) < 7 || len(ip) > 15) {
-		return nil, fmt.Errorf("invalid IPv4 address")
+	if ipVersion == 4 && len(ip) < 7 { //min IPv4 address length is 7 characters
+		return nil, fmt.Errorf("invalid ip address")
 	}
 
-	if ipVersion == 6 && (len(ip) < 2 || len(ip) > 39) {
-		return nil, fmt.Errorf("invalid IPv6 address")
+	if ipVersion == 6 && len(ip) < 2 { //min IPv6 address length is 2 characters
+		return nil, fmt.Errorf("invalid ip address")
 	}
 
-	arr := make([]byte, 32+2+len(ip))
+	arr := make([]byte, 2+len(ip))
 	arr[0] = ipVersion
 	arr[1] = uint8(len(ip))
 	copy(arr[2:], ip)
 
-	_, publicKey, err := curve25519.GenerateCurve25519KeyPair()
-	if err != nil {
-		log.Fatalf("could not generate public key: %s", err)
-	}
-
-	copy(arr[2+len(ip):], publicKey[:])
-
-	return arr, nil
+	return &arr, nil
 }
