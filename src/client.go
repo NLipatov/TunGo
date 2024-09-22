ccb5010a9c886225348ef5f697f93a58ff3717f6
Nikita <6stringsohei@gmail.com>
2024-09-22 16:29:01 +0200

    client now creates a session for traffic encryption

--- src/client.go
+++ src/client.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"crypto/ed25519"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/binary"
 	"etha-tunnel/handshake"
 	"etha-tunnel/network"
 	"etha-tunnel/network/utils"
 	"etha-tunnel/settings/client"
 	"fmt"
+	"golang.org/x/crypto/chacha20poly1305"
+	"golang.org/x/crypto/curve25519"
+	"golang.org/x/crypto/hkdf"
 	"io"
 	"log"
 	"net"
@@ -36,10 +43,11 @@ func main() {
 	defer conn.Close()
 	log.Printf("Connected to server at %s", conf.ServerTCPAddress)
 
-	err = register(conn, conf)
+	session, err := register(conn, conf)
 	if err != nil {
 		log.Fatalf("registration failed: %s", err)
 	}
+	fmt.Print(session)
 
 	go func() {
 		buf := make([]byte, 65535)
@@ -87,27 +95,84 @@ func main() {
 	}
 }
 
-func register(conn net.Conn, conf *client.Conf) error {
-	rm, err := (&handshake.ClientHello{}).Write(4, strings.Split(conf.IfIP, "/")[0])
+func register(conn net.Conn, conf *client.Conf) (*handshake.Session, error) {
+	edPub, ed, err := ed25519.GenerateKey(rand.Reader)
 	if err != nil {
-		return fmt.Errorf("failed to serialize registration message")
+		return nil, fmt.Errorf("failed to generate ed25519 key pair: %s", err)
+	}
+
+	var curvePrivate [32]byte
+	_, _ = io.ReadFull(rand.Reader, curvePrivate[:])
+	curvePublic, _ := curve25519.X25519(curvePrivate[:], curve25519.Basepoint)
+	nonce := make([]byte, 32)
+	_, _ = io.ReadFull(rand.Reader, nonce)
+
+	rm, err := (&handshake.ClientHello{}).Write(4, strings.Split(conf.IfIP, "/")[0], edPub, &curvePublic, &nonce)
+	if err != nil {
+		return nil, fmt.Errorf("failed to serialize registration message")
 	}
 
 	_, err = conn.Write(*rm)
 	if err != nil {
-		return fmt.Errorf("failed to notice server on local address: %v", err)
+		return nil, fmt.Errorf("failed to notice server on local address: %v", err)
 	}
 
 	//Mocked server hello
-	sHBuf := make([]byte, 1)
+	sHBuf := make([]byte, 128)
 	_, err = conn.Read(sHBuf)
 	if err != nil {
-		return fmt.Errorf("failed to read server-hello message")
+		return nil, fmt.Errorf("failed to read server-hello message")
 	}
 
-	log.Printf("registered at %v", conf.IfIP)
+	serverHello, err := (&handshake.ServerHello{}).Read(sHBuf)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read server-hello message")
+	}
 
-	return nil
+	clientConf, err := (&client.Conf{}).Read()
+	if err != nil {
+		return nil, fmt.Errorf("failed to read client configuration: %s", err)
+	}
+	serverEdPub, err := base64.StdEncoding.DecodeString(clientConf.ServerEd25519PublicKey)
+	if err != nil {
+		return nil, fmt.Errorf("invalid ed pub key in configuration: %s", err)
+	}
+	if !ed25519.Verify(serverEdPub, append(append(serverHello.CurvePublicKey, serverHello.ServerNonce...), nonce...), serverHello.ServerSignature) {
+		return nil, fmt.Errorf("server failed signature check")
+	}
+
+	clientDataToSign := append(append(curvePublic, nonce...), serverHello.ServerNonce...)
+	clientSignature := ed25519.Sign(ed, clientDataToSign)
+	cS, err := (&handshake.ClientSignature{}).Write(&clientSignature)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create client signature message: %s", err)
+	}
+
+	_, err = conn.Write(*cS)
+	if err != nil {
+		return nil, fmt.Errorf("failed to send client signature message: %s", err)
+	}
+
+	sharedSecret, _ := curve25519.X25519(curvePrivate[:], serverHello.CurvePublicKey)
+	salt := sha256.Sum256(append(serverHello.ServerNonce, nonce...))
+	infoSC := []byte("server-to-client") // server-key info
+	infoCS := []byte("client-to-server") // client-key info
+	serverToClientHKDF := hkdf.New(sha256.New, sharedSecret, salt[:], infoSC)
+	clientToServerHKDF := hkdf.New(sha256.New, sharedSecret, salt[:], infoCS)
+	keySize := chacha20poly1305.KeySize
+	serverToClientKey := make([]byte, keySize)
+	_, _ = io.ReadFull(serverToClientHKDF, serverToClientKey)
+	clientToServerKey := make([]byte, keySize)
+	_, _ = io.ReadFull(clientToServerHKDF, clientToServerKey)
+
+	clientSession, err := handshake.NewSession(clientToServerKey, serverToClientKey, false)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create client session: %s\n", err)
+	}
+
+	clientSession.SessionId = sha256.Sum256(append(sharedSecret, salt[:]...))
+
+	return clientSession, nil
 }
 
 func configureClient(conf *client.Conf) error {
