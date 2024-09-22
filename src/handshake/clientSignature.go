9a7745242ecd929044ed588dd1058ed770dab31e
Nikita <6stringsohei@gmail.com>
2024-09-22 16:27:07 +0200

    client signature struct

---
+++ src/handshake/clientSignature.go
@@ -0,0 +1,28 @@
+package handshake
+
+import "fmt"
+
+type ClientSignature struct {
+	ClientSignature []byte
+}
+
+func (s *ClientSignature) Read(data []byte) (*ClientSignature, error) {
+	if len(data) < 64 {
+		return nil, fmt.Errorf("invalid data")
+	}
+
+	s.ClientSignature = data[:64]
+
+	return s, nil
+}
+
+func (m *ClientSignature) Write(signature *[]byte) (*[]byte, error) {
+	if len(*signature) != 64 {
+		return nil, fmt.Errorf("invalid signature")
+	}
+
+	arr := make([]byte, len(*signature))
+	copy(arr, *signature)
+
+	return &arr, nil
+}
