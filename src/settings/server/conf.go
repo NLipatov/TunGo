e5863b7e33f391d1341571d6218a2c20d215c8f0
Nikita <6stringsohei@gmail.com>
2024-09-25 19:52:16 +0200

    client conf.go update

--- src/settings/server/conf.go
+++ src/settings/server/conf.go
@@ -1,15 +1,48 @@
 package server
 
 import (
+	"crypto/ed25519"
 	"encoding/json"
 	"os"
 	"path/filepath"
 )
 
 type Conf struct {
-	IfName  string `json:"IfName"`
-	IfIP    string `json:"IfIP"`
-	TCPPort string `json:"TCPPort"`
+	IfName                string             `json:"IfName"`
+	IfIP                  string             `json:"IfIP"`
+	TCPPort               string             `json:"TCPPort"`
+	FallbackServerAddress string             `json:"FallbackServerAddress"`
+	Ed25519PublicKey      ed25519.PublicKey  `json:"Ed25519PublicKey"`
+	Ed25519PrivateKey     ed25519.PrivateKey `json:"Ed25519PrivateKey"`
+}
+
+func (s *Conf) InsertEdKeys(public ed25519.PublicKey, private ed25519.PrivateKey) error {
+	confPath, err := getServerConfPath()
+	if err != nil {
+		return err
+	}
+
+	currentConf, err := s.Read()
+	currentConf.Ed25519PublicKey = public
+	currentConf.Ed25519PrivateKey = private
+
+	jsonContent, err := json.Marshal(currentConf)
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(confPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.Write(jsonContent)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (s *Conf) Read() (*Conf, error) {
