1d4605cfcc74bbabb56127cb8fa94874e3a1aa45
Nikita <6stringsohei@gmail.com>
2024-10-17 19:19:32 +0200

    fixed a problem when keep-alive was constantly sent even if connection is active

--- src/network/keepalive/keepalive.go
+++ src/network/keepalive/keepalive.go
@@ -4,19 +4,17 @@ import (
 	"bytes"
 	"context"
 	"encoding/binary"
-	"fmt"
-	"net"
 	"time"
 )
 
-var keepAlivePacketContent = []byte{'K', 'E', 'E', 'P', 'A', 'L', 'I', 'V', 'E'}
+var keepAlivePacketContent = [9]byte{'K', 'E', 'E', 'P', 'A', 'L', 'I', 'V', 'E'}
 
 func StartConnectionProbing(ctx context.Context, connCancel context.CancelFunc, sendKeepAliveChan chan bool, receiveKeepAliveChan chan bool) {
 	sendInterval := time.Duration(25) * time.Second
 	reconnectInterval := time.Duration(35) * time.Second
 	lastPacket := time.Now()
 
-	ticker := time.NewTicker(sendInterval)
+	ticker := time.NewTicker(sendInterval - sendInterval/4)
 	defer ticker.Stop()
 
 	go func() {
@@ -34,6 +32,10 @@ func StartConnectionProbing(ctx context.Context, connCancel context.CancelFunc, 
 					return
 				}
 
+				if time.Since(lastPacket) < sendInterval {
+					continue
+				}
+
 				sendKeepAliveChan <- true
 			}
 		}
@@ -41,20 +43,14 @@ func StartConnectionProbing(ctx context.Context, connCancel context.CancelFunc, 
 	<-ctx.Done()
 }
 
-func Send(conn net.Conn) error {
-	keepAliveMessage := []byte("KEEPALIVE")
+func Generate() ([]byte, error) {
 	lengthBuf := make([]byte, 4)
-	binary.BigEndian.PutUint32(lengthBuf, uint32(len(keepAliveMessage)))
-	_, keepAliveWriteErr := conn.Write(append(lengthBuf, keepAliveMessage...))
-	if keepAliveWriteErr != nil {
-		return fmt.Errorf("failed to send keep alive: %s", keepAliveWriteErr)
-	}
-
-	return nil
+	binary.BigEndian.PutUint32(lengthBuf, uint32(len(keepAlivePacketContent)))
+	return append(lengthBuf, keepAlivePacketContent[:]...), nil
 }
 
 func IsKeepAlive(data []byte) bool {
-	if bytes.Equal(data, keepAlivePacketContent) {
+	if bytes.Equal(data, keepAlivePacketContent[:]) {
 		return true
 	}
 	return false
