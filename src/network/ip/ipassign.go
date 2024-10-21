f4a2cd176f007b806b825f150dd0c13c253ad199
Nikita <6stringsohei@gmail.com>
2024-10-21 18:56:29 +0200

    ipassign fixes and improvements

--- src/network/ip/ipassign.go
+++ src/network/ip/ipassign.go
@@ -6,8 +6,12 @@ import (
 	"net"
 )
 
-func ipToUint32(ip net.IP) uint32 {
-	return binary.BigEndian.Uint32(ip.To4())
+func ipToUint32(ip net.IP) (uint32, error) {
+	ip4 := ip.To4()
+	if ip4 == nil {
+		return 0, fmt.Errorf("invalid IPv4 address")
+	}
+	return binary.BigEndian.Uint32(ip4), nil
 }
 
 func uint32ToIP(ip uint32) net.IP {
@@ -22,10 +26,13 @@ func AllocateServerIp(subnetCIDR string) (string, error) {
 		return "", fmt.Errorf("invalid subnet: %v", err)
 	}
 
-	ipUint := ipToUint32(ip)
+	ipUint, err := ipToUint32(ip)
+	if err != nil {
+		return "", fmt.Errorf("invalid subnet: %v", err)
+	}
 	serverIp := uint32ToIP(ipUint + 1) //server ip is always a first ip of subnetwork range
 
-	return serverIp.String(), nil
+	return fmt.Sprintf("%s", serverIp.String()), nil
 }
 
 func AllocateClientIp(subnetCIDR string, clientCounter int) (string, error) {
@@ -34,7 +41,11 @@ func AllocateClientIp(subnetCIDR string, clientCounter int) (string, error) {
 		return "", fmt.Errorf("invalid subnet: %v", err)
 	}
 
-	ipUint := ipToUint32(ip)
+	ipUint, err := ipToUint32(ip)
+	if err != nil {
+		return "", fmt.Errorf("invalid subnet: %v", err)
+	}
+
 	maskSize, bits := network.Mask.Size()
 	availableAddresses := 1 << (bits - maskSize)
 
@@ -47,9 +58,33 @@ func AllocateClientIp(subnetCIDR string, clientCounter int) (string, error) {
 
 	nextIP := uint32ToIP(nextIPUint)
 
-	if nextIP.Equal(network.IP) || nextIP.Equal(net.IPv4bcast) {
+	if nextIP.Equal(network.IP) || isBroadcastAddress(nextIP, network.Mask) {
 		return "", fmt.Errorf("generated IP is invalid (network or broadcast address)")
 	}
 
 	return nextIP.String(), nil
+}
+
+func ToCIDR(subnetCIDR string, addressInSubnet string) (string, error) {
+	ip := net.ParseIP(addressInSubnet)
+	if ip == nil {
+		return "", fmt.Errorf("invalid IP address: %s", addressInSubnet)
+	}
+
+	_, network, err := net.ParseCIDR(subnetCIDR)
+	if err != nil {
+		return "", fmt.Errorf("invalid subnet: %v", err)
+	}
+	ones, _ := network.Mask.Size()
+
+	return fmt.Sprintf("%s/%d", ip.String(), ones), nil
+}
+
+func isBroadcastAddress(ip net.IP, mask net.IPMask) bool {
+	network := ip.Mask(mask)
+	broadcast := make(net.IP, len(network))
+	for i := range network {
+		broadcast[i] = network[i] | ^mask[i]
+	}
+	return ip.Equal(broadcast)
 }
