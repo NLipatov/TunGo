ea17a759a3342981863f31e5bbd2250c2f88e2ab
Nikita <6stringsohei@gmail.com>
2024-10-28 17:14:44 +0100

    client side update

---
+++ src/client/forwarding/clienttcptunforward/udp.go
@@ -0,0 +1,133 @@
+package clienttcptunforward
+
+import (
+	"context"
+	"etha-tunnel/handshake/ChaCha20"
+	"etha-tunnel/network"
+	"etha-tunnel/network/keepalive"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func TunToUDP(conn *net.UDPConn, tunFile *os.File, session *ChaCha20.Session, ctx context.Context, connCancel context.CancelFunc, sendKeepAliveChan chan bool) {
+	buf := make([]byte, maxPacketLengthBytes)
+	connWriteChan := make(chan []byte, getConnWriteBufferSize())
+
+	//writes whatever comes from chan to UDP
+	go func() {
+		for {
+			select {
+			case <-ctx.Done(): // Stop-signal
+				return
+			case data := <-connWriteChan:
+				_, err := conn.Write(data)
+				if err != nil {
+					log.Printf("write to UDP failed: %s", err)
+					connCancel()
+					return
+				}
+			}
+		}
+	}()
+
+	//passes keepalive messages to chan
+	go func() {
+		for {
+			select {
+			case <-ctx.Done(): // Stop-signal
+				return
+			case <-sendKeepAliveChan:
+				data, err := keepalive.Generate()
+				if err != nil {
+					log.Println(err)
+				}
+				select {
+				case connWriteChan <- data:
+				case <-ctx.Done():
+					return
+				}
+			}
+		}
+	}()
+
+	//passes anything from tun to chan
+	for {
+		select {
+		case <-ctx.Done(): // Stop-signal
+			return
+		default:
+			n, err := tunFile.Read(buf)
+			if err != nil {
+				if ctx.Err() != nil {
+					fmt.Printf("context ended with error: %s\n", err)
+					return
+				}
+				log.Printf("failed to read from TUN: %v", err)
+				continue
+			}
+
+			encryptedPacket, err := session.Encrypt(buf[:n])
+			if err != nil {
+				log.Printf("failed to encrypt packet: %v", err)
+				continue
+			}
+
+			packet, err := (&network.Packet{}).Encode(encryptedPacket)
+			if err != nil {
+				log.Printf("packet encoding failed: %s", err)
+				continue
+			}
+
+			select {
+			case connWriteChan <- packet.Payload:
+			case <-ctx.Done():
+				return
+			}
+		}
+	}
+}
+
+func UDPToTun(conn *net.UDPConn, tunFile *os.File, session *ChaCha20.Session, ctx context.Context, connCancel context.CancelFunc, receiveKeepAliveChan chan bool) {
+	buf := make([]byte, maxPacketLengthBytes)
+	for {
+		select {
+		case <-ctx.Done(): // Stop-signal
+			return
+		default:
+			n, _, err := conn.ReadFromUDP(buf)
+			if err != nil {
+				if ctx.Err() != nil {
+					fmt.Printf("context ended with error: %s\n", err)
+					return
+				}
+				log.Printf("read from UDP failed: %v", err)
+				connCancel()
+				return
+			}
+
+			packet, err := (&network.Packet{}).Decode(buf[:n], session)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+
+			select {
+			case receiveKeepAliveChan <- true:
+				if packet.Length == 9 && keepalive.IsKeepAlive(packet.Payload) {
+					log.Println("keep-alive: OK")
+					continue
+				}
+			default:
+			}
+
+			// Write the decrypted packet to the TUN interface
+			_, err = tunFile.Write(packet.Payload)
+			if err != nil {
+				log.Printf("failed to write to TUN: %v", err)
+				return
+			}
+		}
+	}
+}
--- src/client/forwarding/routing/udp.go
+++ src/client/forwarding/routing/udp.go
@@ -2,13 +2,17 @@ package routing
 
 import (
 	"context"
+	"etha-tunnel/client/forwarding/clienttcptunforward"
 	"etha-tunnel/client/forwarding/ipconfiguration"
+	"etha-tunnel/handshake/ChaCha20"
 	"etha-tunnel/handshake/ChaCha20/handshakeHandlers"
+	"etha-tunnel/network/keepalive"
 	"etha-tunnel/settings"
 	"fmt"
 	"log"
 	"net"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -31,21 +35,51 @@ func StartUDPRouting(settings settings.ConnectionSettings, tunFile *os.File, ctx
 			log.Println("connection is aborted")
 			return err
 		}
-		log.Println("sessionId: %s", session.SessionId)
+
+		// Create a child context for managing data forwarding goroutines
+		connCtx, connCancel := context.WithCancel(*ctx)
+		var wg sync.WaitGroup
+		wg.Add(2)
+
+		// Start a goroutine to monitor context cancellation and close the connection
+		go func() {
+			<-connCtx.Done()
+			conn.Close()
+			return
+		}()
+
+		go startUDPForwarding(conn, tunFile, session, &connCtx, &connCancel, &wg)
+
+		// Wait for goroutines to finish
+		wg.Wait()
+
+		// After goroutines finish, check if shutdown was initiated
+		if (*ctx).Err() != nil {
+			log.Println("Client is shutting down.")
+			return err
+		} else {
+			// Connection lost unexpectedly, attempt to reconnect
+			log.Println("Connection lost, attempting to reconnect...")
+		}
+
+		// Close the connection (if not already closed)
+		conn.Close()
 	}
 }
 
-func establishUDPConnection(settings settings.ConnectionSettings, ctx context.Context) (net.Conn, error) {
+func establishUDPConnection(settings settings.ConnectionSettings, ctx context.Context) (*net.UDPConn, error) {
 	reconnectAttempts := 0
 	backoff := initialBackoff
 
 	for {
 		serverAddr := fmt.Sprintf("%s%s", settings.ConnectionIP, settings.ConnectionPort)
-		dialer := &net.Dialer{}
-		dialCtx, dialCancel := context.WithTimeout(ctx, connectionTimeout)
-		defer dialCancel()
 
-		conn, err := dialer.DialContext(dialCtx, "udp", serverAddr)
+		udpAddr, err := net.ResolveUDPAddr("udp", serverAddr)
+		if err != nil {
+			return nil, fmt.Errorf("failed to resolve UDP address: %w", err)
+		}
+
+		conn, err := net.DialUDP("udp", nil, udpAddr)
 		if err != nil {
 			log.Printf("Failed to connect to server: %v", err)
 			reconnectAttempts++
@@ -71,4 +105,22 @@ func establishUDPConnection(settings settings.ConnectionSettings, ctx context.Co
 
 		return conn, nil
 	}
+}
+
+func startUDPForwarding(conn *net.UDPConn, tunFile *os.File, session *ChaCha20.Session, connCtx *context.Context, connCancel *context.CancelFunc, wg *sync.WaitGroup) {
+	sendKeepAliveCommandChan := make(chan bool, 1)
+	connPacketReceivedChan := make(chan bool, 1)
+	go keepalive.StartConnectionProbing(*connCtx, *connCancel, sendKeepAliveCommandChan, connPacketReceivedChan)
+
+	// TUN -> TCP
+	go func() {
+		defer wg.Done()
+		clienttcptunforward.TunToUDP(conn, tunFile, session, *connCtx, *connCancel, sendKeepAliveCommandChan)
+	}()
+
+	// TCP -> TUN
+	go func() {
+		defer wg.Done()
+		clienttcptunforward.UDPToTun(conn, tunFile, session, *connCtx, *connCancel, connPacketReceivedChan)
+	}()
 }
