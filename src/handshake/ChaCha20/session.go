90de8af1e8a7aac6bc699eedde7791429404656d
Nikita <6stringsohei@gmail.com>
2024-10-31 18:37:16 +0100

    moved nonce to a separate file

--- src/handshake/ChaCha20/session.go
+++ src/handshake/ChaCha20/session.go
@@ -4,18 +4,15 @@ import (
 	"crypto/cipher"
 	"fmt"
 	"golang.org/x/crypto/chacha20poly1305"
-	"sync/atomic"
 )
 
 type Session struct {
-	sendCipher    cipher.AEAD
-	recvCipher    cipher.AEAD
-	SendNonceLow  uint64 // Lower 8 bytes of the nonce for encryption
-	SendNonceHigh uint32 // Upper 4 bytes of the nonce for encryption
-	RecvNonceLow  uint64 // Lower 8 bytes of the nonce for decryption
-	RecvNonceHigh uint32 // Upper 4 bytes of the nonce for decryption
-	isServer      bool
-	SessionId     [32]byte
+	sendCipher cipher.AEAD
+	recvCipher cipher.AEAD
+	SendNonce  *Nonce
+	RecvNonce  *Nonce
+	isServer   bool
+	SessionId  [32]byte
 }
 
 func NewSession(sendKey, recvKey []byte, isServer bool) (*Session, error) {
@@ -30,18 +27,16 @@ func NewSession(sendKey, recvKey []byte, isServer bool) (*Session, error) {
 	}
 
 	return &Session{
-		sendCipher:    sendCipher,
-		recvCipher:    recvCipher,
-		SendNonceLow:  0,
-		SendNonceHigh: 0,
-		RecvNonceLow:  0,
-		RecvNonceHigh: 0,
-		isServer:      isServer,
+		sendCipher: sendCipher,
+		recvCipher: recvCipher,
+		RecvNonce:  NewNonce(),
+		SendNonce:  NewNonce(),
+		isServer:   isServer,
 	}, nil
 }
 
 func (s *Session) Encrypt(plaintext []byte) ([]byte, error) {
-	nonce, err := s.incrementNonce(&s.SendNonceLow, &s.SendNonceHigh)
+	nonce, err := s.SendNonce.incrementNonce()
 	if err != nil {
 		return nil, err
 	}
@@ -52,7 +47,7 @@ func (s *Session) Encrypt(plaintext []byte) ([]byte, error) {
 }
 
 func (s *Session) Decrypt(ciphertext []byte) ([]byte, error) {
-	nonce, err := s.incrementNonce(&s.RecvNonceLow, &s.RecvNonceHigh)
+	nonce, err := s.RecvNonce.incrementNonce()
 	if err != nil {
 		return nil, err
 	}
@@ -75,32 +70,4 @@ func (s *Session) CreateAAD(isServerToClient bool, nonce []byte) []byte {
 	aad := append(s.SessionId[:], direction...)
 	aad = append(aad, nonce...)
 	return aad
-}
-
-func (s *Session) incrementNonce(low *uint64, high *uint32) ([]byte, error) {
-	// Ensure nonce does not overflow
-	if atomic.LoadUint32(high) == ^uint32(0) && atomic.LoadUint64(low) == ^uint64(0) {
-		return nil, fmt.Errorf("nonce overflow: maximum number of messages reached")
-	}
-
-	nonce := make([]byte, 12)
-
-	if atomic.LoadUint64(low) == ^uint64(0) {
-		atomic.AddUint32(high, 1)
-		atomic.StoreUint64(low, 0)
-	} else {
-		atomic.AddUint64(low, 1)
-	}
-
-	lowVal := atomic.LoadUint64(low)
-	highVal := atomic.LoadUint32(high)
-
-	for i := 0; i < 8; i++ {
-		nonce[i] = byte(lowVal >> (8 * i))
-	}
-	for i := 0; i < 4; i++ {
-		nonce[8+i] = byte(highVal >> (8 * i))
-	}
-
-	return nonce, nil
 }
