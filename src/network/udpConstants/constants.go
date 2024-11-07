42df1f85ce1fafdbffacd6246482539ff1bc0bf3
Nikita <6stringsohei@gmail.com>
2024-11-07 15:43:23 +0100

    udpConstants update

--- src/network/udpConstants/constants.go
+++ src/network/udpConstants/constants.go
@@ -1,18 +1,14 @@
 package udpConstants
 
 const (
-	// MaxDatagramSize RFC 768: max UDP datagram size is 65 535 bytes.
-	MaxDatagramSize = 65_535
+	standardMTU   = 1500
+	UDPHeaderSize = 8
+	IPHeaderSize  = 20
+	NonceSize     = 12
+	TotalOverhead = UDPHeaderSize + IPHeaderSize + NonceSize
 
-	// MaxTunPacketSize guarantees that UDP TunGo's packet will not exceed 65_507
-	// (65_507 bytes is max datagram size minus IP and headers).
-	// Max TunGo client transmittable UDP packet will be: 65_535 - 8 - 20 - 12 = 65495 bytes, where:
-	// - 65_535 - max UDP datagram;
-	// - 8 bytes - UDP headers;
-	// - 20 bytes - IP;
-	// - 12 bytes - nonce;
-	MaxTunPacketSize = MaxDatagramSize - 8 - 20 - 12
+	MaxTunPacketSize = standardMTU - TotalOverhead
 
-	// MaxUdpPacketSize is length of MaxTunPacketSize + 12 bytes - because we need large enough buffer to read TunPacket and additional 12 bytes of nonce.
-	MaxUdpPacketSize = MaxTunPacketSize + 12
+	// MaxUdpPacketSize is 1472 bytes. Length of 1472 includes 12 bytes of ChaCha20 nonce.
+	MaxUdpPacketSize = MaxTunPacketSize + NonceSize
 )
