6a07f95510a0c0292d9161427c7421cbba68cf9f
Nikita Lipatov <6stringsohei@gmail.com>
2024-12-12 16:43:32 +0100

    minor changes

--- src/crypto/chacha20/constants.go
+++ src/crypto/chacha20/constants.go
@@ -1,13 +1,13 @@
 package chacha20
 
 const (
+	lengthHeaderLength   = 2
 	signatureLength      = 64
 	nonceLength          = 32
 	curvePublicKeyLength = 32
+	minIpLength          = 4
 	maxIpLength          = 39
 
-	// MaxClientHelloSizeBytes consist of: 39(max ip) + 2(length headers) + 32(ed25519 pub key) + 32(curve pub key) + 32(nonce)
-	MaxClientHelloSizeBytes = maxIpLength + 2 + curvePublicKeyLength + curvePublicKeyLength + nonceLength
-	// minClientHelloSizeBytes consist of: 4(min ip) + 2(length headers) + 32(ed25519 pub key) + 32(curve pub key) + 32(nonce)
-	minClientHelloSizeBytes = 4 + 2 + curvePublicKeyLength + curvePublicKeyLength + nonceLength
+	MaxClientHelloSizeBytes = maxIpLength + lengthHeaderLength + curvePublicKeyLength + curvePublicKeyLength + nonceLength
+	minClientHelloSizeBytes = minIpLength + lengthHeaderLength + curvePublicKeyLength + curvePublicKeyLength + nonceLength
 )
