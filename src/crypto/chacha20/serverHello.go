1563b2767ae18abfb7377115f481cdac0652d67f
Nikita Lipatov <6stringsohei@gmail.com>
2024-12-12 16:24:49 +0100

    minor changes

--- src/crypto/chacha20/serverHello.go
+++ src/crypto/chacha20/serverHello.go
@@ -2,36 +2,43 @@ package chacha20
 
 import "fmt"
 
+const (
+	signatureLength      = 64
+	nonceLength          = 32
+	curvePublicKeyLength = 32
+)
+
 type ServerHello struct {
-	ServerSignature []byte
-	ServerNonce     []byte
-	CurvePublicKey  []byte
+	Signature      []byte
+	Nonce          []byte
+	CurvePublicKey []byte
 }
 
 func (sH *ServerHello) Read(data []byte) (*ServerHello, error) {
-	if len(data) < 128 {
+	if len(data) < signatureLength+nonceLength+curvePublicKeyLength {
 		return nil, fmt.Errorf("invalid data")
 	}
 
-	sH.ServerSignature = data[:64]
-	sH.ServerNonce = data[64 : 64+32]
-	sH.CurvePublicKey = data[64+32 : 128]
+	sH.Signature = data[:signatureLength]
+	sH.Nonce = data[signatureLength : signatureLength+nonceLength]
+	sH.CurvePublicKey = data[signatureLength+nonceLength : signatureLength+nonceLength+curvePublicKeyLength]
 
 	return sH, nil
 }
 
 func (sH *ServerHello) Write(signature *[]byte, nonce *[]byte, curvePublicKey *[]byte) (*[]byte, error) {
-	if len(*signature) != 64 {
+	if len(*signature) != signatureLength {
 		return nil, fmt.Errorf("invalid signature")
 	}
-	if len(*nonce) != 32 {
+	if len(*nonce) != nonceLength {
 		return nil, fmt.Errorf("invalid nonce")
 	}
-
-	if len(*curvePublicKey) != 32 {
+	if len(*curvePublicKey) != curvePublicKeyLength {
 		return nil, fmt.Errorf("invalid curve public key")
 	}
-	arr := make([]byte, len(*signature)+len(*nonce)+len(*curvePublicKey))
+
+	arr := make([]byte, signatureLength+nonceLength+curvePublicKeyLength)
+
 	copy(arr, *signature)
 	copy(arr[len(*signature):], *nonce)
 	copy(arr[len(*signature)+len(*nonce):], *curvePublicKey)
