2f2c01093759baccc49e92bca8e22fb5e340705d
Nikita <6stringsohei@gmail.com>
2025-03-03 17:42:43 +0100

    inplace CreateAAD

--- src/crypto/chacha20/tcp_session.go
+++ src/crypto/chacha20/tcp_session.go
@@ -10,13 +10,15 @@ import (
 )
 
 type TcpSession struct {
-	sendCipher cipher.AEAD
-	recvCipher cipher.AEAD
-	SendNonce  *Nonce
-	RecvNonce  *Nonce
-	isServer   bool
-	SessionId  [32]byte
-	nonceBuf   *NonceBuf
+	sendCipher       cipher.AEAD
+	recvCipher       cipher.AEAD
+	SendNonce        *Nonce
+	RecvNonce        *Nonce
+	isServer         bool
+	SessionId        [32]byte
+	nonceBuf         *NonceBuf
+	encryptionAadBuf []byte
+	decryptionAadBuf []byte
 }
 
 func DeriveSessionId(sharedSecret []byte, salt []byte) ([32]byte, error) {
@@ -42,13 +44,15 @@ func NewTcpSession(id [32]byte, sendKey, recvKey []byte, isServer bool) (*TcpSes
 	}
 
 	return &TcpSession{
-		SessionId:  id,
-		sendCipher: sendCipher,
-		recvCipher: recvCipher,
-		RecvNonce:  NewNonce(),
-		SendNonce:  NewNonce(),
-		isServer:   isServer,
-		nonceBuf:   nil,
+		SessionId:        id,
+		sendCipher:       sendCipher,
+		recvCipher:       recvCipher,
+		RecvNonce:        NewNonce(),
+		SendNonce:        NewNonce(),
+		isServer:         isServer,
+		nonceBuf:         nil,
+		encryptionAadBuf: make([]byte, 80),
+		decryptionAadBuf: make([]byte, 80),
 	}, nil
 }
 
@@ -69,7 +73,7 @@ func (s *TcpSession) Encrypt(plaintext []byte) ([]byte, error) {
 
 	nonceBytes := s.SendNonce.Encode()
 
-	aad := s.CreateAAD(s.isServer, nonceBytes)
+	aad := s.CreateAAD(s.isServer, nonceBytes, s.encryptionAadBuf)
 	ciphertext := s.sendCipher.Seal(plaintext[:0], nonceBytes, plaintext, aad)
 
 	return ciphertext, nil
@@ -83,7 +87,7 @@ func (s *TcpSession) Decrypt(ciphertext []byte) ([]byte, error) {
 
 	nonceBytes := s.RecvNonce.Encode()
 
-	aad := s.CreateAAD(!s.isServer, nonceBytes)
+	aad := s.CreateAAD(!s.isServer, nonceBytes, s.decryptionAadBuf)
 	plaintext, err := s.recvCipher.Open(ciphertext[:0], nonceBytes, ciphertext, aad)
 	if err != nil {
 		// Properly handle failed decryption attempt to avoid reuse of any state
@@ -93,13 +97,14 @@ func (s *TcpSession) Decrypt(ciphertext []byte) ([]byte, error) {
 	return plaintext, nil
 }
 
-func (s *TcpSession) CreateAAD(isServerToClient bool, nonce []byte) []byte {
+func (s *TcpSession) CreateAAD(isServerToClient bool, nonce, aad []byte) []byte {
 	direction := []byte("client-to-server")
 	if isServerToClient {
 		direction = []byte("server-to-client")
 	}
 
-	aad := append(s.SessionId[:], direction...)
-	aad = append(aad, nonce...)
-	return aad
+	copy(aad, s.SessionId[:])
+	copy(aad[len(s.SessionId):], direction)
+	copy(aad[len(s.SessionId)+len(direction):], nonce)
+	return aad[:len(s.SessionId)+len(direction)+len(nonce)]
 }
