fd7731568c41d1be8b5e3d008c4e19dfa59e1b3c
Nikita <6stringsohei@gmail.com>
2025-03-09 16:36:59 +0100

    minor code changes / code simplified

--- src/client/tun_configurator/contracts.go
+++ src/client/tun_configurator/contracts.go
@@ -7,7 +7,7 @@ import (
 
 type TunConfigurator interface {
 	// Configure creates a TunAdapter that can be used as TUN-like device
-	Configure(s settings.ConnectionSettings) network.TunAdapter
+	Configure(s settings.ConnectionSettings) (network.TunAdapter, error)
 	// Deconfigure performs de-configuration logic to suspend a TUN-like device
 	Deconfigure(s settings.ConnectionSettings)
 }
