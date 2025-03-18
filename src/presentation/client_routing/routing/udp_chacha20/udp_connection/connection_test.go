e025005f122bb567bd2994b5d3baa0cc7f6ac573
Nikita Lipatov <6stringsohei@gmail.com>
2025-03-18 18:31:43 +0100

    udp connection test

---
+++ src/presentation/client_routing/routing/udp_chacha20/udp_connection/connection_test.go
@@ -0,0 +1,67 @@
+package udp_connection
+
+import (
+	"fmt"
+	"net"
+	"testing"
+	"time"
+	"tungo/settings"
+)
+
+func TestDefaultConnection_Establish(t *testing.T) {
+	port := 3001
+	testSettings := settings.ConnectionSettings{
+		ConnectionIP: "127.0.0.1",
+		Port:         fmt.Sprintf("%d", port),
+	}
+
+	serverAcceptChan := make(chan struct{}, 1)
+	serverErrChan := make(chan error, 1)
+
+	addr := net.UDPAddr{
+		IP:   net.ParseIP("127.0.0.1"),
+		Port: port,
+	}
+
+	listener, err := net.ListenUDP("udp", &addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer func(listener *net.UDPConn) {
+		_ = listener.Close()
+	}(listener)
+
+	go func() {
+
+		for {
+			buffer := make([]byte, 1)
+			_, _, acceptErr := listener.ReadFromUDP(buffer)
+			if acceptErr != nil {
+				serverErrChan <- acceptErr
+				return
+			}
+			serverAcceptChan <- struct{}{}
+			return
+		}
+	}()
+
+	connection := NewConnection(testSettings)
+	conn, connErr := connection.Establish()
+	if connErr != nil {
+		t.Fatal(connErr)
+	}
+	_, writeErr := conn.Write([]byte{1})
+	if writeErr != nil {
+		t.Fatal(writeErr)
+	}
+
+	select {
+	case <-time.After(time.Duration(1) * time.Second):
+		t.Fatalf("timeout")
+	case serverErr := <-serverErrChan:
+		t.Fatalf("server error: %s", serverErr)
+	case <-serverAcceptChan:
+		return
+	}
+}
