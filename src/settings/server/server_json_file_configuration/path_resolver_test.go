154a3721b454b61303dba510eb9222459de57705
Nikita <6stringsohei@gmail.com>
2025-03-19 22:54:22 +0300

    pathResolver minor refactoring and test

---
+++ src/settings/server/server_json_file_configuration/path_resolver_test.go
@@ -0,0 +1,25 @@
+package server_json_file_configuration
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestResolveSuccess(t *testing.T) {
+	resolver := newPathResolver()
+	actual, err := resolver.resolve()
+	if err != nil {
+		t.Fatalf("resolve() returned error: %v", err)
+	}
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("os.Getwd() returned error: %v", err)
+	}
+
+	expected := filepath.Join(filepath.Dir(cwd), "src", "settings", "server", "conf.json")
+	if actual != expected {
+		t.Errorf("expected path %q, actual %q", expected, actual)
+	}
+}
