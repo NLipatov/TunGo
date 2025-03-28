647eeb1f43dbffb012fae2959b78fd8396ae98f3
Nikita <6stringsohei@gmail.com>
2025-03-28 19:55:24 +0100

    minor changes in router.go

--- src/presentation/client_routing/routing/udp_chacha20/router.go
+++ src/presentation/client_routing/routing/udp_chacha20/router.go
@@ -2,6 +2,7 @@ package udp_chacha20
 
 import (
 	"context"
+	"errors"
 	"log"
 	"net"
 	"sync"
@@ -39,12 +40,9 @@ func (r *UDPRouter) RouteTraffic(ctx context.Context) error {
 	// TUN -> UDP
 	go func() {
 		defer wg.Done()
-		tunWorker := newUdpWorker(r.conn, r.tun, r.cryptographyService)
-
-		handlingErr := tunWorker.HandleTun(routingCtx, routingCancel)
-
-		if handlingErr != nil {
-			log.Printf("TUN -> UDP error: %v", handlingErr)
+		worker := newUdpWorker(r.conn, r.tun, r.cryptographyService)
+		if err := worker.HandleTun(ctx, routingCancel); err != nil && !errors.Is(err, context.Canceled) {
+			log.Printf("TUN -> UDP error: %v", err)
 			routingCancel()
 			return
 		}
@@ -53,12 +51,9 @@ func (r *UDPRouter) RouteTraffic(ctx context.Context) error {
 	// UDP -> TUN
 	go func() {
 		defer wg.Done()
-		tunWorker := newUdpWorker(r.conn, r.tun, r.cryptographyService)
-
-		handlingErr := tunWorker.HandleConn(routingCtx, routingCancel)
-
-		if handlingErr != nil {
-			log.Printf("UDP -> TUN error: %v", handlingErr)
+		worker := newUdpWorker(r.conn, r.tun, r.cryptographyService)
+		if err := worker.HandleConn(ctx, routingCancel); err != nil && !errors.Is(err, context.Canceled) {
+			log.Printf("UDP -> TUN error: %v", err)
 			routingCancel()
 			return
 		}
