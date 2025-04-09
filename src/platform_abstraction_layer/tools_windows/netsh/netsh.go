496b01e35e56ae461d0bf54101e5ca05dbd65b1f
Nikita Lipatov <6stringsohei@gmail.com>
2025-04-09 15:39:35 +0300

    avaitable netsh operations

--- src/platform_abstraction_layer/tools_windows/netsh/netsh.go
+++ src/platform_abstraction_layer/tools_windows/netsh/netsh.go
@@ -1,45 +1,60 @@
 package netsh
 
 import (
+	"fmt"
 	"os/exec"
 	"strconv"
 )
 
 func RouteDelete(hostIP string) error {
-	return exec.Command("route", "delete", hostIP).Run()
+	output, err := exec.Command("route", "delete", hostIP).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("RouteDelete error: %v, output: %s", err, output)
+	}
+	return nil
 }
 
 func InterfaceIPSetAddressStatic(interfaceName, hostIP, mask, gateway string) error {
-	return exec.
-		Command("netsh", "interface", "ip", "set", "address",
-			"name="+interfaceName, "static", hostIP, mask, gateway, "1").
-		Run()
+	output, err := exec.Command("netsh", "interface", "ip", "set", "address",
+		"name="+interfaceName, "static", hostIP, mask, gateway, "1").CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("InterfaceIPSetAddressStatic error: %v, output: %s", err, output)
+	}
+	return nil
 }
 
 func InterfaceIPV4AddRouteDefault(interfaceName, gateway string) error {
-	return exec.
-		Command("netsh", "interface", "ipv4", "add", "route", "0.0.0.0/0",
-			"name="+interfaceName, gateway, "metric=1").
-		Run()
+	output, err := exec.Command("netsh", "interface", "ipv4", "add", "route", "0.0.0.0/0",
+		"name="+interfaceName, gateway, "metric=1").CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("InterfaceIPV4AddRouteDefault error: %v, output: %s", err, output)
+	}
+	return nil
 }
 
 func InterfaceIPV4DeleteAddress(IfName string) error {
-	return exec.
-		Command("netsh", "interface", "ipv4", "delete", "route", "0.0.0.0/0",
-			"name="+IfName).
-		Run()
+	output, err := exec.Command("netsh", "interface", "ipv4", "delete", "route", "0.0.0.0/0",
+		"name="+IfName).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("InterfaceIPV4DeleteAddress error: %v, output: %s", err, output)
+	}
+	return nil
 }
 
 func InterfaceIPDeleteAddress(IfName, IfAddr string) error {
-	return exec.
-		Command("netsh", "interface", "ip", "delete", "address",
-			"name="+IfName, "addr="+IfAddr).
-		Run()
+	output, err := exec.Command("netsh", "interface", "ip", "delete", "address",
+		"name="+IfName, "addr="+IfAddr).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("InterfaceIPDeleteAddress error: %v, output: %s", err, output)
+	}
+	return nil
 }
 
 func SetInterfaceMetric(interfaceName string, metric int) error {
-	return exec.
-		Command("netsh", "interface", "ipv4", "set", "interface",
-			interfaceName, "metric="+strconv.Itoa(metric)).
-		Run()
+	output, err := exec.Command("netsh", "interface", "ipv4", "set", "interface",
+		interfaceName, "metric="+strconv.Itoa(metric)).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("SetInterfaceMetric error: %v, output: %s", err, output)
+	}
+	return nil
 }
