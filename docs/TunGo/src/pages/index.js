5593ec11aba76764e9a7ca29941e491463bfbc0a
Nikita Lipatov <6stringsohei@gmail.com>
2025-04-21 15:46:09 +0200

    added systemd unit deployment option guide

--- docs/TunGo/src/pages/index.js
+++ docs/TunGo/src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/QuickStart">
+            to="/docs/intro">
               Setup VPN Tunnel in 10 sec ⏱️
           </Link>
         </div>
