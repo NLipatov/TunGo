db66bab2113768cb37e781d30789db3e4193d3ad
Nikita Lipatov <6stringsohei@gmail.com>
2025-05-01 20:03:22 +0200

    minor improvement

--- src/infrastructure/cryptography/chacha20/handshake/crypto.go
+++ src/infrastructure/cryptography/chacha20/handshake/crypto.go
@@ -3,7 +3,11 @@ package handshake
 import (
 	"crypto/ed25519"
 	"crypto/rand"
+	"crypto/sha256"
+	"fmt"
+	"golang.org/x/crypto/chacha20poly1305"
 	"golang.org/x/crypto/curve25519"
+	"golang.org/x/crypto/hkdf"
 	"io"
 )
 
@@ -13,6 +17,10 @@ type crypto interface {
 	GenerateEd25519KeyPair() (ed25519.PublicKey, ed25519.PrivateKey, error)
 	GenerateX25519KeyPair() ([]byte, [32]byte, error)
 	GenerateRandomBytesArray(size int) []byte
+	GenerateChaCha20Keys(
+		curvePrivate,
+		serverNonce []byte,
+		extHello ClientHello) (sessionId [32]byte, serverToClientKey, clientToServerKey []byte, err error)
 }
 
 type defaultCrypto struct {
@@ -50,4 +58,37 @@ func (c *defaultCrypto) GenerateRandomBytesArray(size int) []byte {
 	randomSalt := make([]byte, size)
 	_, _ = io.ReadFull(rand.Reader, randomSalt)
 	return randomSalt
+}
+
+func (c *defaultCrypto) GenerateChaCha20Keys(
+	curvePrivate,
+	serverNonce []byte,
+	extHello ClientHello) (sessionId [32]byte, serverToClientKey, clientToServerKey []byte, err error) {
+	// Generate shared secret and salt
+	sharedSecret, _ := curve25519.X25519(curvePrivate[:], extHello.curvePublicKey)
+	salt := sha256.Sum256(append(serverNonce, extHello.clientNonce...))
+
+	infoSC := []byte("server-to-client") // server-key info
+	infoCS := []byte("client-to-server") // client-key info
+
+	// Generate HKDF for both encryption directions
+	serverToClientHKDF := hkdf.New(sha256.New, sharedSecret, salt[:], infoSC)
+	clientToServerHKDF := hkdf.New(sha256.New, sharedSecret, salt[:], infoCS)
+	keySize := chacha20poly1305.KeySize
+
+	// Generate keys for both encryption directions
+	serverToClientKey = make([]byte, keySize)
+	_, _ = io.ReadFull(serverToClientHKDF, serverToClientKey)
+	clientToServerKey = make([]byte, keySize)
+	_, _ = io.ReadFull(clientToServerHKDF, clientToServerKey)
+
+	sessionId, deriveSessionIdErr := deriveSessionId(sharedSecret, salt[:])
+	if deriveSessionIdErr != nil {
+		return [32]byte{},
+			nil,
+			nil,
+			fmt.Errorf("failed to derive session id: %s", deriveSessionIdErr)
+	}
+
+	return sessionId, serverToClientKey, clientToServerKey, nil
 }
