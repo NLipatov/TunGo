ce9a8d1eb0b5647781ad47698b4ce9af95d12f87
Nikita Lipatov <6stringsohei@gmail.com>
2025-05-01 19:46:08 +0200

    minor improvement

--- src/infrastructure/cryptography/chacha20/handshake/handshake.go
+++ src/infrastructure/cryptography/chacha20/handshake/handshake.go
@@ -1,18 +1,12 @@
 package handshake
 
 import (
-	"crypto/sha256"
 	"fmt"
-	"golang.org/x/crypto/curve25519"
-	"io"
 	"net"
 	"tungo/application"
 	"tungo/settings"
 	"tungo/settings/client_configuration"
 	"tungo/settings/server_configuration"
-
-	"golang.org/x/crypto/chacha20poly1305"
-	"golang.org/x/crypto/hkdf"
 )
 
 const (
@@ -58,17 +52,6 @@ func (h *HandshakeImpl) ServerKey() []byte {
 
 func (h *HandshakeImpl) ServerSideHandshake(conn application.ConnectionAdapter) (*string, error) {
 	c := newDefaultCrypto()
-	serverConfigurationManager := server_configuration.NewManager(server_configuration.NewServerResolver())
-	conf, err := serverConfigurationManager.Configuration()
-	if err != nil {
-		return nil, fmt.Errorf("failed to read server configuration: %s", err)
-	}
-
-	handshake := NewServerHandshake(conn)
-	clientHello, clientHelloErr := handshake.ReceiveClientHello()
-	if clientHelloErr != nil {
-		return nil, clientHelloErr
-	}
 
 	// Generate server hello response
 	curvePublic, curvePrivate, curveErr := c.GenerateX25519KeyPair()
@@ -77,53 +60,35 @@ func (h *HandshakeImpl) ServerSideHandshake(conn application.ConnectionAdapter) 
 	}
 	serverNonce := c.GenerateRandomBytesArray(32)
 
+	serverConfigurationManager := server_configuration.NewManager(server_configuration.NewServerResolver())
+	conf, err := serverConfigurationManager.Configuration()
+	if err != nil {
+		return nil, fmt.Errorf("failed to read server configuration: %s", err)
+	}
+
+	//handshake process starts here
+	handshake := NewServerHandshake(conn)
+	clientHello, clientHelloErr := handshake.ReceiveClientHello()
+	if clientHelloErr != nil {
+		return nil, clientHelloErr
+	}
+
 	serverHelloErr := handshake.SendServerHello(c, conf.Ed25519PrivateKey, serverNonce, curvePublic, clientHello.clientNonce)
 	if serverHelloErr != nil {
 		return nil, serverHelloErr
 	}
 
-	clientSignatureBuf := make([]byte, 64)
-
-	// Read client signature
-	_, err = conn.Read(clientSignatureBuf)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read client signature: %s\n", err)
-	}
-	var clientSignature Signature
-	signatureErr := clientSignature.UnmarshalBinary(clientSignatureBuf)
+	signatureErr := handshake.VerifyClientSignature(c, clientHello, serverNonce)
 	if signatureErr != nil {
 		return nil, signatureErr
 	}
 
-	// Verify client signature
-	if !c.Verify(clientHello.edPublicKey, append(append(clientHello.curvePublicKey, clientHello.clientNonce...), serverNonce...), clientSignature.Signature) {
-		return nil, fmt.Errorf("client signature verification failed")
+	sessionId, clientToServerKey, serverToClientKey, sessionKeysErr := handshake.CalculateKeys(curvePrivate[:], serverNonce, clientHello)
+	if sessionKeysErr != nil {
+		return nil, sessionKeysErr
 	}
 
-	// Generate shared secret and salt
-	sharedSecret, _ := curve25519.X25519(curvePrivate[:], clientHello.curvePublicKey)
-	salt := sha256.Sum256(append(serverNonce, clientHello.clientNonce...))
-
-	infoSC := []byte("server-to-client") // server-key info
-	infoCS := []byte("client-to-server") // client-key info
-
-	// Generate HKDF for both encryption directions
-	serverToClientHKDF := hkdf.New(sha256.New, sharedSecret, salt[:], infoSC)
-	clientToServerHKDF := hkdf.New(sha256.New, sharedSecret, salt[:], infoCS)
-	keySize := chacha20poly1305.KeySize
-
-	// Generate keys for both encryption directions
-	serverToClientKey := make([]byte, keySize)
-	_, _ = io.ReadFull(serverToClientHKDF, serverToClientKey)
-	clientToServerKey := make([]byte, keySize)
-	_, _ = io.ReadFull(clientToServerHKDF, clientToServerKey)
-
-	derivedSessionId, deriveSessionIdErr := deriveSessionId(sharedSecret, salt[:])
-	if deriveSessionIdErr != nil {
-		return nil, fmt.Errorf("failed to derive session id: %s", derivedSessionId)
-	}
-
-	h.id = derivedSessionId
+	h.id = [32]byte(sessionId)
 	h.clientKey = clientToServerKey
 	h.serverKey = serverToClientKey
 
--- src/infrastructure/cryptography/chacha20/handshake/server_handshake.go
+++ src/infrastructure/cryptography/chacha20/handshake/server_handshake.go
@@ -2,6 +2,12 @@ package handshake
 
 import (
 	"crypto/ed25519"
+	"crypto/sha256"
+	"fmt"
+	"golang.org/x/crypto/chacha20poly1305"
+	"golang.org/x/crypto/curve25519"
+	"golang.org/x/crypto/hkdf"
+	"io"
 	"tungo/application"
 )
 
@@ -48,4 +54,59 @@ func (h *ServerHandshake) SendServerHello(
 
 	_, writeErr := h.conn.Write(marshalledServerHello)
 	return writeErr
+}
+
+func (h *ServerHandshake) VerifyClientSignature(c crypto, hello ClientHello, serverNonce []byte) error {
+	clientSignatureBuf := make([]byte, 64)
+
+	// Read client signature
+	_, readErr := h.conn.Read(clientSignatureBuf)
+	if readErr != nil {
+		return readErr
+	}
+	var clientSignature Signature
+	unmarshalErr := clientSignature.UnmarshalBinary(clientSignatureBuf)
+	if unmarshalErr != nil {
+		return unmarshalErr
+	}
+
+	// Verify client signature
+	if !c.Verify(hello.edPublicKey, append(append(hello.curvePublicKey, hello.clientNonce...), serverNonce...), clientSignature.Signature) {
+		return fmt.Errorf("client failed signature verification")
+	}
+
+	return nil
+}
+
+func (h *ServerHandshake) CalculateKeys(
+	curvePrivate,
+	serverNonce []byte,
+	hello ClientHello) (sessionId, clientToServerKey, serverToClientKey []byte, err error) {
+	// Generate shared secret and salt
+	sharedSecret, _ := curve25519.X25519(curvePrivate[:], hello.curvePublicKey)
+	salt := sha256.Sum256(append(serverNonce, hello.clientNonce...))
+
+	infoSC := []byte("server-to-client") // server-key info
+	infoCS := []byte("client-to-server") // client-key info
+
+	// Generate HKDF for both encryption directions
+	serverToClientHKDF := hkdf.New(sha256.New, sharedSecret, salt[:], infoSC)
+	clientToServerHKDF := hkdf.New(sha256.New, sharedSecret, salt[:], infoCS)
+	keySize := chacha20poly1305.KeySize
+
+	// Generate keys for both encryption directions
+	serverToClientKey = make([]byte, keySize)
+	_, _ = io.ReadFull(serverToClientHKDF, serverToClientKey)
+	clientToServerKey = make([]byte, keySize)
+	_, _ = io.ReadFull(clientToServerHKDF, clientToServerKey)
+
+	derivedSessionId, deriveSessionIdErr := deriveSessionId(sharedSecret, salt[:])
+	if deriveSessionIdErr != nil {
+		return nil,
+			nil,
+			nil,
+			fmt.Errorf("failed to derive session id: %s", derivedSessionId)
+	}
+
+	return sessionId, clientToServerKey, serverToClientKey, nil
 }
