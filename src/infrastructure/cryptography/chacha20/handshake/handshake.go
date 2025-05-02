a58af72b3d68bc07b0c4db1bab26ee3e61509624
Nikita Lipatov <6stringsohei@gmail.com>
2025-05-02 15:48:31 +0200

    minor improvement

--- src/infrastructure/cryptography/chacha20/handshake/handshake.go
+++ src/infrastructure/cryptography/chacha20/handshake/handshake.go
@@ -103,8 +103,6 @@ func (h *DefaultHandshake) ClientSideHandshake(conn net.Conn, settings settings.
 		return fmt.Errorf("failed to read client configuration: %s", generateKeyErr)
 	}
 
-	clientCrypto := NewDefaultClientCrypto()
-
 	edPublicKey, edPrivateKey, generateKeyErr := c.GenerateEd25519KeyPair()
 	if generateKeyErr != nil {
 		return fmt.Errorf("failed to generate ed25519 key pair: %s", generateKeyErr)
@@ -135,7 +133,7 @@ func (h *DefaultHandshake) ClientSideHandshake(conn net.Conn, settings settings.
 		return sendSignatureErr
 	}
 
-	serverToClientKey, clientToServerKey, derivedSessionId, calculateKeysErr := clientCrypto.CalculateKeys(sessionPrivateKey[:], sessionSalt, serverHello.nonce, serverHello.curvePublicKey)
+	derivedSessionId, clientToServerKey, serverToClientKey, calculateKeysErr := c.GenerateChaCha20KeysServerside(sessionPrivateKey[:], sessionSalt, &serverHello)
 	if calculateKeysErr != nil {
 		return calculateKeysErr
 	}
