8e55bd159253483f4b312e22b32766472f5c1afb
Nikita <6stringsohei@gmail.com>
2025-05-06 18:34:29 +0200

    ConnectionAdapter interface extended with 2 new methods: SetReadDeadline(t time.Time) error, SetWriteDeadline(t time.Time) error

--- src/application/connection_adapter.go
+++ src/application/connection_adapter.go
@@ -1,8 +1,12 @@
 package application
 
+import "time"
+
 // ConnectionAdapter provides a single and trivial API for any supported transports
 type ConnectionAdapter interface {
 	Write([]byte) (int, error)
 	Read([]byte) (int, error)
 	Close() error
+	SetReadDeadline(t time.Time) error
+	SetWriteDeadline(t time.Time) error
 }
