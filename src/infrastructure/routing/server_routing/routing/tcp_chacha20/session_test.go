461040df21961c3629488a33c31ab712dbf80037
Nikita Lipatov <6stringsohei@gmail.com>
2025-05-12 14:41:35 +0200

    tcp session test

---
+++ src/infrastructure/routing/server_routing/routing/tcp_chacha20/session_test.go
@@ -0,0 +1,43 @@
+package tcp_chacha20
+
+import (
+	"bytes"
+	"net"
+	"testing"
+	"time"
+)
+
+type sessionTestConn struct{}
+
+func (d *sessionTestConn) Read([]byte) (int, error)           { return 0, nil }
+func (d *sessionTestConn) Write([]byte) (int, error)          { return 0, nil }
+func (d *sessionTestConn) Close() error                       { return nil }
+func (d *sessionTestConn) LocalAddr() net.Addr                { return nil }
+func (d *sessionTestConn) RemoteAddr() net.Addr               { return nil }
+func (d *sessionTestConn) SetDeadline(_ time.Time) error      { return nil }
+func (d *sessionTestConn) SetReadDeadline(_ time.Time) error  { return nil }
+func (d *sessionTestConn) SetWriteDeadline(_ time.Time) error { return nil }
+
+type sessionTestCrypto struct{}
+
+func (d *sessionTestCrypto) Encrypt(b []byte) ([]byte, error) { return b, nil }
+func (d *sessionTestCrypto) Decrypt(b []byte) ([]byte, error) { return b, nil }
+
+func TestSessionAccessors(t *testing.T) {
+	internal := []byte{10, 0, 1, 3}
+	external := []byte{93, 184, 216, 34}
+
+	s := session{
+		conn:                &sessionTestConn{},
+		CryptographyService: &sessionTestCrypto{},
+		internalIP:          internal,
+		externalIP:          external,
+	}
+
+	if got := s.InternalIP(); !bytes.Equal(got, internal) {
+		t.Errorf("InternalIP() = %v, want %v", got, internal)
+	}
+	if got := s.ExternalIP(); !bytes.Equal(got, external) {
+		t.Errorf("ExternalIP() = %v, want %v", got, external)
+	}
+}
