156dbab349114a3dd074436e0360f4658a44ff16
Nikita Lipatov <6stringsohei@gmail.com>
2025-06-12 16:55:01 +0200

    server side worker refactoring

---
+++ src/infrastructure/routing/server_routing/routing/udp_chacha20/transport_handler.go
@@ -0,0 +1,172 @@
+package udp_chacha20
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/netip"
+	"tungo/application"
+	"tungo/infrastructure/PAL/server_configuration"
+	"tungo/infrastructure/cryptography/chacha20"
+	"tungo/infrastructure/cryptography/chacha20/handshake"
+	"tungo/infrastructure/network"
+	"tungo/infrastructure/routing/server_routing/session_management"
+	"tungo/infrastructure/settings"
+)
+
+type TransportHandler struct {
+	ctx            context.Context
+	settings       settings.Settings
+	writer         io.Writer
+	sessionManager session_management.WorkerSessionManager[session]
+}
+
+func NewTransportHandler(
+	ctx context.Context,
+	settings settings.Settings,
+	writer io.Writer,
+	sessionManager session_management.WorkerSessionManager[session],
+) application.TransportHandler {
+	return &TransportHandler{
+		ctx:            ctx,
+		settings:       settings,
+		writer:         writer,
+		sessionManager: sessionManager,
+	}
+}
+
+func (t *TransportHandler) HandleTransport() error {
+	addr, err := net.ResolveUDPAddr("udp", net.JoinHostPort("", t.settings.Port))
+	if err != nil {
+		log.Fatalf("failed to resolve udp address: %s", err)
+		return err
+	}
+
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		log.Fatalf("failed to listen on port: %s", err)
+	}
+	defer func(conn net.Conn) {
+		_ = conn.Close()
+	}(conn)
+
+	log.Printf("server listening on port %s (UDP)", t.settings.Port)
+
+	go func() {
+		<-t.ctx.Done()
+		_ = conn.Close()
+	}()
+
+	dataBuf := make([]byte, network.MaxPacketLengthBytes+12)
+	oobBuf := make([]byte, 1024)
+	destinationAddressBuf := [4]byte{}
+
+	for {
+		select {
+		case <-t.ctx.Done():
+			return nil
+		default:
+			n, _, _, clientAddr, readFromUdpErr := conn.ReadMsgUDPAddrPort(dataBuf, oobBuf)
+			if readFromUdpErr != nil {
+				if t.ctx.Done() != nil {
+					return nil
+				}
+
+				log.Printf("failed to read from UDP: %s", readFromUdpErr)
+				continue
+			}
+
+			destinationAddressBuf = clientAddr.Addr().Unmap().As4()
+			clientSession, getErr := t.sessionManager.GetByExternalIP(destinationAddressBuf[:])
+			if getErr != nil || clientSession.remoteAddrPort.Port() != clientAddr.Port() {
+				// Pass initial data to registration function
+				regErr := t.registerClient(conn, clientAddr, dataBuf[:n])
+				if regErr != nil {
+					log.Printf("host %v failed registration: %v", clientAddr.Addr().AsSlice(), regErr)
+					_, _ = conn.WriteToUDPAddrPort([]byte{
+						byte(network.SessionReset),
+					}, clientAddr)
+				}
+				continue
+			}
+
+			// Handle client data
+			decrypted, decryptionErr := clientSession.CryptographyService.Decrypt(dataBuf[:n])
+			if decryptionErr != nil {
+				log.Printf("failed to decrypt data: %v", decryptionErr)
+				continue
+			}
+
+			// Write the decrypted packet to the TUN interface
+			_, err = t.writer.Write(decrypted)
+			if err != nil {
+				log.Printf("failed to write to TUN: %v", err)
+			}
+		}
+	}
+}
+
+func (t *TransportHandler) registerClient(conn *net.UDPConn, clientAddr netip.AddrPort, initialData []byte) error {
+	_ = conn.SetReadBuffer(65536)
+	_ = conn.SetWriteBuffer(65536)
+
+	// Pass initialData and clientAddr to the crypto function
+	h := handshake.NewHandshake()
+	adapter := network.NewInitialDataAdapter(
+		network.NewUdpAdapter(conn, clientAddr), initialData)
+	internalIP, handshakeErr := h.ServerSideHandshake(adapter)
+	if handshakeErr != nil {
+		return handshakeErr
+	}
+
+	serverConfigurationManager := server_configuration.NewManager(server_configuration.NewServerResolver())
+	_, err := serverConfigurationManager.Configuration()
+	if err != nil {
+		return fmt.Errorf("failed to read server configuration: %s", err)
+	}
+
+	cryptoSession, cryptoSessionErr := chacha20.NewUdpSession(h.Id(), h.ServerKey(), h.ClientKey(), true)
+	if cryptoSessionErr != nil {
+		return cryptoSessionErr
+	}
+
+	remoteAddr, remoteParseErr := netip.ParseAddrPort(clientAddr.String())
+	if remoteParseErr != nil {
+		return fmt.Errorf("invalid client address: %v", clientAddr)
+	}
+
+	t.sessionManager.Add(session{
+		connectionAdapter: &network.ServerUdpAdapter{
+			UdpConn:  conn,
+			AddrPort: clientAddr,
+		},
+		remoteAddrPort:      remoteAddr,
+		CryptographyService: cryptoSession,
+		internalIP:          t.extractIPv4(internalIP),
+		externalIP:          t.extractIPv4(clientAddr.Addr().Unmap().AsSlice()),
+	})
+
+	log.Printf("%v registered as: %v", clientAddr.Addr().As4(), internalIP)
+
+	return nil
+}
+
+func (t *TransportHandler) extractIPv4(ip net.IP) []byte {
+	if len(ip) == 16 && t.isIPv4Mapped(ip) {
+		return ip[12:16]
+	}
+	if len(ip) == 4 {
+		return ip
+	}
+	return nil
+}
+
+func (t *TransportHandler) isIPv4Mapped(ip net.IP) bool {
+	// check for ::ffff:0:0/96 prefix
+	return ip[0] == 0 && ip[1] == 0 && ip[2] == 0 &&
+		ip[3] == 0 && ip[4] == 0 && ip[5] == 0 &&
+		ip[6] == 0 && ip[7] == 0 && ip[8] == 0 &&
+		ip[9] == 0 && ip[10] == 0xff && ip[11] == 0xff
+}
