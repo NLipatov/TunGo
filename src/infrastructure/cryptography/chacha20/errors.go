4efae0249f9a16d5cdd8ea07d252186aa6fbc129
Nikita Lipatov <6stringsohei@gmail.com>
2025-07-10 12:32:19 +0200

    ErrInvalidNonceSize added

--- src/infrastructure/cryptography/chacha20/errors.go
+++ src/infrastructure/cryptography/chacha20/errors.go
@@ -4,4 +4,7 @@ import (
 	"errors"
 )
 
-var ErrNonUniqueNonce = errors.New("critical decryption error: nonce was not unique")
+var (
+	ErrNonUniqueNonce   = errors.New("critical decryption error: nonce was not unique")
+	ErrInvalidNonceSize = errors.New("critical decryption error: invalid nonce size")
+)
