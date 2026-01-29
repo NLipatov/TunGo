d5c2369c7d0a3cc7dffacee9bd7456b275c15cc4
Nikita Lipatov <6stringsohei@gmail.com>
2026-01-29 14:12:34 +0300

    refactoring: build fix

--- src/infrastructure/routing/server_routing/routing/tcp_chacha20/session.go
+++ src/infrastructure/routing/server_routing/routing/tcp_chacha20/session.go
@@ -13,21 +13,21 @@ type Session struct {
 	// internalIP is the client's VPN-assigned IPv4 address (e.g. 10.0.1.3).
 	internalIP netip.Addr
 	// externalIP is the client's real-world IPv4 address (e.g. 51.195.101.45) and port (e.g. 1754).
-	externalIP      netip.AddrPort
-	rekeyController *rekey.StateMachine
+	externalIP netip.AddrPort
+	fsm        rekey.FSM
 }
 
 func NewSession(
 	connectionAdapter connection.Transport,
 	cryptographyService connection.Crypto,
-	rekeyController *rekey.StateMachine,
+	fsm rekey.FSM,
 	internalIP netip.Addr,
 	externalIP netip.AddrPort,
 ) connection.Session {
 	return &Session{
 		connectionAdapter:   connectionAdapter,
 		cryptographyService: cryptographyService,
-		rekeyController:     rekeyController,
+		fsm:                 fsm,
 		internalIP:          internalIP,
 		externalIP:          externalIP,
 	}
@@ -45,8 +45,8 @@ func (s Session) Crypto() connection.Crypto {
 	return s.cryptographyService
 }
 
-func (s Session) RekeyController() *rekey.StateMachine {
-	return s.rekeyController
+func (s Session) RekeyController() rekey.FSM {
+	return s.fsm
 }
 
 func (s Session) Transport() connection.Transport {
