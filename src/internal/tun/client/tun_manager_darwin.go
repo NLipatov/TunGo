f157c89993262df3a4981db38dce5161b0f7a658
Nikita Lipatov <6stringsohei@gmail.com>
2026-08-19 21:31:43 +0400

    refactor(tun): remove ineffective Darwin stale cleanup

--- src/internal/tun/client/tun_manager_darwin.go
+++ src/internal/tun/client/tun_manager_darwin.go
@@ -3,13 +3,9 @@
 package client
 
 import (
-	"errors"
-	"fmt"
 	"io"
-	"log/slog"
 	"net/netip"
 	"tungo/internal/config/client"
-	"tungo/internal/config/settings"
 	"tungo/internal/tun/internal/darwin/manager"
 )
 
@@ -18,52 +14,10 @@ type clientManager interface {
 	CloseTunnel() error
 }
 
-type Manager struct {
-	manager       clientManager
-	configuration *client.Configuration
-}
-
-func New(conf *client.Configuration) (*Manager, error) {
-	connectionSettings, err := conf.ActiveSettings()
+func New(conf *client.Configuration) (clientManager, error) {
+	settings, err := conf.ActiveSettings()
 	if err != nil {
 		return nil, err
 	}
-	concrete, err := manager.New(connectionSettings)
-	if err != nil {
-		return nil, err
-	}
-	return &Manager{
-		manager:       concrete,
-		configuration: conf,
-	}, nil
-}
-
-func (m *Manager) OpenTunnel(serverAddr netip.Addr) (io.ReadWriteCloser, error) {
-	return m.manager.OpenTunnel(serverAddr)
-}
-
-func (m *Manager) CloseTunnel() error {
-	activeErr := m.manager.CloseTunnel()
-	active, err := m.configuration.ActiveSettings()
-	if err != nil {
-		return errors.Join(activeErr, fmt.Errorf("determine active TUN settings: %w", err))
-	}
-	m.disposeStale(m.configuration.TCPSettings, active.TunName)
-	m.disposeStale(m.configuration.UDPSettings, active.TunName)
-	m.disposeStale(m.configuration.WSSettings, active.TunName)
-	return activeErr
-}
-
-func (m *Manager) disposeStale(s settings.Settings, activeTunName string) {
-	if s.TunName == "" || s.TunName == activeTunName {
-		return
-	}
-	cleanupManager, err := manager.New(s)
-	if err != nil {
-		slog.Warn("failed to prepare stale TUN cleanup", "name", s.TunName, "err", err)
-		return
-	}
-	if err := cleanupManager.CloseTunnel(); err != nil {
-		slog.Warn("failed to clean stale TUN configuration", "name", s.TunName, "err", err)
-	}
+	return manager.New(settings)
 }
